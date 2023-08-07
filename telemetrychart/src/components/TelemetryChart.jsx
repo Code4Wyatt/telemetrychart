530c16856983410c31b42631b206bd9b91e38fca
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2023-08-07 20:06:27 +0100

    add hook to parse csv file

---
+++ telemetrychart/src/components/TelemetryChart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TelemetryChart() {
+  return (
+    <div>TelemetryChart</div>
+  )
+}
+
+export default TelemetryChart
---
+++ telemetrychart/src/hooks/parseCsv.ts
@@ -0,0 +1,12 @@
+import Papa from 'papaparse';
+
+const parseCsv = (csvFile, callback) => {
+    Papa.parse(csvFile, {
+        download: true,
+        header: true,
+        dynamicTyping: true,
+        complete: (results) => {
+            callback(results.data);
+        }
+    })
+}
